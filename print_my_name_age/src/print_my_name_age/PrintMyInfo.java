79fc8c91da57b4b919e8759eca33baef238425c7
eastdh <eastdh@gmail.com>
2026-07-12 02:59:49 +0900

    TEST: home commit

--- print_my_name_age/src/print_my_name_age/PrintMyInfo.java
+++ print_my_name_age/src/print_my_name_age/PrintMyInfo.java
@@ -2,7 +2,8 @@ package print_my_name_age;
 
 public class PrintMyInfo {
 
-	public static void main(String[] args) {
-		System.out.println("내 이름은 \"유동혁\"이고, 나이는 \"29\"세 입니다.");
-	}
+  public static void main(String[] args) {
+    System.out.println("내 이름은 \"유동혁\"이고, 나이는 \"29\"세 입니다.");
+
+  }
 }
