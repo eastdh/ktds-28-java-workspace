cd0561abc4489349f388910fbb8f3fa39e9f5ef9
eastdh <eastdh@gmail.com>
2026-07-17 04:53:13 +0900

    과제 완료

--- workflow/src/workflow/MethodHW0716.java
+++ workflow/src/workflow/MethodHW0716.java
@@ -3,7 +3,7 @@ package workflow;
 public class MethodHW0716 {
   // 1. 정수형 변수 2개를 파라미터로 받아, 값을 출력하는 메소드.
   public static void print2Param(int a, int b) {
-    System.out.println(a + ", " + b);
+    System.out.println(a + " " + b);
   }
 
   // 2. 정수형 변수 2개를 파라미터로 받아, 합계를 출력하는 메소드.
@@ -70,9 +70,9 @@ public class MethodHW0716 {
 
   // 9. 정수 배열을 파라미터로 받아, 2,5,8 배수인 숫자만 출력하는 메소드.
   public static void print258InArray(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
+    for (int i = 1; i < arr.length; i++) {
       if (arr[i] % 2 == 0 || arr[i] % 5 == 0) {
-        System.out.print(arr[i] + ", ");
+        System.out.print(arr[i] + " ");
       }
     }
     System.out.println();
@@ -94,51 +94,173 @@ public class MethodHW0716 {
 
   // 12. 정수형 변수 1개를 파라미터로 받아, 4부터 정수형 변수까지의 범위 중 소수(Prime Number)만 출력하는 메소드.
   public static void print4toNPrimeOnly(int n) {
+    // 각 인덱스 값으로 배열 초기화
+    int[] primes = new int[n + 1];
+    for (int i = 2; i < primes.length; i++) {
+      primes[i] = i;
+    }
 
+    // 소수의 배수들을 전부 0으로 바꿈
+    for (int i = 2; i < primes.length; i++) {
+      if (primes[i] == 0) {
+        continue;
+      }
+      for (int j = 2; i * j <= n; j++) {
+        primes[i * j] = 0;
+      }
+    }
 
+    // 출력
+    for (int i = 4; i < primes.length; i++) {
+      if (primes[i] != 0) {
+        System.out.print(i + " ");
+      }
+    }
+    System.out.println();
   }
 
   // 13. 정수형 배열 변수 1개와 정수형 변수 1개를 파라미터로 받아, 해당 배열의 정수형변수 인덱스에 의 값을 반환하는 메소드.
-  //
   // -> 예> 값(길이가 5인 배열, 2) ==> 배열의 2번 인덱스의 값
-  //
   // -> 예> 값(길이가 5인 배열, 4) ==> 배열의 4번 인덱스의 값
-  //
   // -> 예> 값(길이가 5인 배열, 5) ==> 0
-  //
   // -> 예> 값(길이가 5인 배열, -1) ==> 0
-  //
   // -> 예> 값(길이가 5인 배열, 1) ==> 배열의 1번 인덱스의 값
-  //
   // -> 예> 값(길이가 5인 배열, 0) ==> 배열의 0번 인덱스의 값
-  //
   // -> 예> 값(길이가 5인 배열, 3) ==> 배열의 3번 인덱스의 값
-  //
   // -> 예> 값(길이가 5인 배열, 7) ==> 0
-  //
+  public static int arrayNthValueReturn(int[] arr, int n) {
+    if (n >= 0 && n < arr.length) {
+      return arr[n];
+    } else {
+      return 0;
+    }
+  }
+
   // 14. 정수형 배열 변수 1개를 파라미터로 받아, 가장 처음 나오는 3의 배수만 반환하는 메소드
-  //
   // - 정수형 배열 변수내부에 3의 배수가 없을 경우 -1 을 반환.
-  //
+  public static int first3NReturn(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > 0 && arr[i] % 3 == 0) {
+        return arr[i];
+      }
+    }
+    return -1;
+  }
+
   // 15. 정수형 배열 변수 2개를 파라미터로 받아, 각 배열에 중복값만 출력하는 메소드.
-  //
   // -> 예> 중복([1,2,3,4,5], [9,7,454,1,2,3]) ==> 1 2 3
-  //
-  // -> 예> 중복([1,2,3,4,5], [1,2,3,4,5]) ==> ""
-  //
+  // -> 예> 중복([1,2,3,4,5], [1,2,3,4,5]) ==> 1 2 3 4 5
+  public static void printDuplicate(int[] a, int[] b) {
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < b.length; j++) {
+        if (a[i] == b[j]) {
+          System.out.print(a[i] + " ");
+          break;
+        }
+      }
+    }
+    System.out.println();
+  }
+
   // 16. 정수형 배열 변수 2개를 파라미터로 받아, 각 배열에 중복되지 않는 값만 출력하는 메소드.
-  //
   // -> 예> 고유([1,2,3,4,5], [9,7,454,1,2,3]) ==> 4, 5, 9. 7, 454
-  //
   // -> 예> 고유([1,2,3,4,5], [1,2,3,4,5]) ==> ""
-  //
+  public static void printUnique(int[] a, int[] b) {
+    for (int i = 0; i < a.length; i++) {
+      boolean isUnique = true;
+      for (int j = 0; j < b.length; j++) {
+        if (a[i] == b[j]) {
+          isUnique = false;
+        }
+      }
+      if (isUnique) {
+        System.out.print(a[i] + " ");
+      }
+    }
+  }
+
+  public static void printAllUnique(int[] a, int[] b) {
+    printUnique(a, b);
+    printUnique(b, a);
+    System.out.println();
+  }
+
   // 17. 정수형 배열 변수 1개를 파라미터로 받아, 모든 값들을 배수로 만드는(반환X) 메소드.
-  //
   // -> 호출 이후에 main 메소드 내부에서 배열 내부의 값들을 모두 출력
+  public static void doubleAll(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] *= 2;
+    }
+  }
+
   public static void main(String[] args) {
+    System.out.println("1 -----------------"); // 1.
+    print2Param(3, 7);
+
+    System.out.println("2 -----------------"); // 2.
+    sum2Param(7, 11);
+
+    System.out.println("3 -----------------"); // 3.
+    System.out.println("3 + 5 + 7 = " + sum3Param(3, 5, 7));
+
+    System.out.println("4 -----------------"); // 4.
+    div2ParamPrint(10, 3);
+
+    System.out.println("5 -----------------"); // 5.
+    System.out.println("10 / 3 = " + div2ParamReturn(10, 3));
+
+    System.out.println("6 -----------------"); // 6.
+    System.out.println(changeDecimalPlaces(10.33333333, 0));
+    System.out.println(changeDecimalPlaces(10.33333333, 1));
     System.out.println(changeDecimalPlaces(10.33333333, 3));
 
+    System.out.println("7 -----------------"); // 7.
+    double max2Double = max2DoubleReturn(10.9, 10.13);
+    System.out.println("max(10.9, 10.13): " + max2Double);
+
+    System.out.println("8 -----------------"); // 8.
+    double min4Double = min4DoubleReturn(10.1, 10.11, 10.9, 10.05);
+    System.out.println("min(10.1, 10.11, 10.9, 10.05): " + min4Double);
+
+    System.out.println("9 -----------------"); // 9.
+    int[] arr50 = new int[50];
+    for (int i = 0; i < arr50.length; i++) {
+      arr50[i] = i;
+    }
+    print258InArray(arr50);
+
+    System.out.println("10 -----------------"); // 10.
     printStringN("aaaa", 5);
+
+    System.out.println("11 -----------------"); // 11.
+    printNDan(7);
+
+    System.out.println("12 -----------------"); // 12.
+    System.out.print("primes -> ");
+    print4toNPrimeOnly(100);
+
+    System.out.println("13 -----------------"); // 13.
+    System.out.println(arrayNthValueReturn(arr50, 2));
+    System.out.println(arrayNthValueReturn(arr50, 35));
+    System.out.println(arrayNthValueReturn(arr50, 100));
+
+    System.out.println("14 -----------------"); // 14.
+    System.out.println(first3NReturn(arr50));
+
+    System.out.println("15 -----------------"); // 15.
+    int[] arrA = {1, 2, 3, 4, 5};
+    int[] arrB = {9, 7, 454, 1, 2, 3};
+    printDuplicate(arrA, arrB);
+
+    System.out.println("16 -----------------"); // 16.
+    printAllUnique(arrA, arrB);
+
+    System.out.println("17 -----------------"); // 17.
+    int[] arr2X = {1, 2, 3, 4, 5};
+    doubleAll(arr2X);
+    for (int i = 0; i < arr2X.length; i++) {
+      System.out.print(arr2X[i] + " ");
+    }
 
   }
 }
