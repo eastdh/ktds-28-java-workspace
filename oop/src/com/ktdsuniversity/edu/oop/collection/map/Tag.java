15eb0b46fb54cc15ae8054f037abdb73dcae403d
eastdh <eastdh@gmail.com>
2026-07-31 10:10:44 +0900

    0731 map 실습

--- oop/src/com/ktdsuniversity/edu/oop/collection/map/Tag.java
+++ oop/src/com/ktdsuniversity/edu/oop/collection/map/Tag.java
@@ -1,6 +1,8 @@
 package com.ktdsuniversity.edu.oop.collection.map;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Tag {
@@ -41,6 +43,50 @@ public class Tag {
     int discount = (int) tags.getOrDefault("할인가", 0); // null 방지 2
     System.out.println(discount);
 
+
+    // Map을 쓰면 안 되는 이유?!
+    List<Map<String, Object>> clothes = new ArrayList<>();
+
+    Map<String, Object> tags1 = new HashMap<String, Object>();
+    tags1.put("판매가격", 15000);
+    tags1.put("소재", "나이론");
+    tags1.put("사이즈", 30);
+    clothes.add(tags1);
+
+    Map<String, Object> tags2 = new HashMap<String, Object>();
+    tags2.put("가격", 16000);
+    tags2.put("소재", "폴리");
+    tags2.put("size", 32);
+    clothes.add(tags2);
+
+    Map<String, Object> tags3 = new HashMap<String, Object>();
+    tags3.put("가격", 5000);
+    tags3.put("소재", "면");
+    tags3.put("사이즈", 50);
+    clothes.add(tags3);
+
+    for (int i = 0; i < clothes.size(); i++) {
+      printTags(clothes.get(i));
+
+    }
   }
 
+  public static void printTags(Map<String, Object> tags) {
+    System.out.println("소재: " + tags.get("소재"));
+    System.out.println("가격: " + tags.get("가격"));
+    System.out.println("사이즈: " + tags.get("사이즈"));
+  }
+
+  /*
+   * 소재: 나이론
+   * 가격: null
+   * 사이즈: 30
+   * 소재: 폴리
+   * 가격: 16000
+   * 사이즈: null
+   * 소재: 면
+   * 가격: 5000
+   * 사이즈: 50
+   * 
+   */
 }
